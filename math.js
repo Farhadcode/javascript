ea72592475bf22cb72cef909503d69e50a94255c
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-01 19:08:13 +0600

    Math addad

---
+++ math.js
@@ -0,0 +1,42 @@
+// var productPrice1 = 12;
+// var productPrice2 = 77;
+// var sum = productPrice1 + productPrice2 ;
+// var  multiy = productPrice2 * productPrice2;
+// var substration = productPrice1 - productPrice2;
+// var divietion = productPrice2 / productPrice1;
+// var modulus = productPrice2 % productPrice1;
+
+// console.log(sum);
+// console.log(multiy);
+// console.log(substration);
+// console.log(divietion);
+// console.log(modulus);
+
+// console.log(Math.round(divietion));
+
+// console.log(Math.abs(substration));
+ 
+var number = 9 ;
+ var randomValu = Math.random(number)
+ var result = randomValu*100;
+console.log(Math.round(result));
+
+console.log(Math.sqrt(144));
+
+//  Math.pow(x: number, y: number) X The base value Y— The exponent value  (x =6 to the power Y =2)
+var x = 6;
+var y = 2 ;
+console.log(Math.pow(x,y));
+
+            // valu ++ & --
+// var age = 21;
+// age = age +1;
+// age += 1;
+//  age++;
+//  console.log(age);
+// var valu = 101
+// valu = valu -1;
+// valu -= 1
+// valu--;
+
+// console.log(valu);
--- variable.js
+++ variable.js
@@ -1,7 +1,11 @@
 var num1 = 877;
 console.log(typeof(num1));
+
 var name = "farhad"
  sum = num1 + name;
+
 console.log(typeof(sum));
+
+
  var valu = true;
 console.log(typeof(valu));
