472ad5de34adf41167f1add529cb3db3047115cf
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-02 23:39:13 +0600

    add new

--- array.js
+++ array.js
@@ -8,6 +8,11 @@ var numberList = [77 ,78 ,23 ,55 , 78 ];
 var lanth =numberList.length;
 var arry = numberList.indexOf(78);
 
-console.log(arry);
-console.log(lanth);
 
+ numberList.push(99);
+numberList.pop();
+numberList.join(33);
+
+ console.log(lanth);
+ console.log(arry);
+console.log(numberList);
