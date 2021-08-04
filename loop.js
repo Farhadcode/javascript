8005c2a2c13b31d20ac5da8c9ff6952c10c56d88
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-05 02:07:38 +0600

    loop add

---
+++ loop.js
@@ -0,0 +1,79 @@
+//   while Loop 
+
+var phonGiven = 0;
+while (phonGiven <= 10) {
+    console.log(" yeahh phon paygase");
+    phonGiven++;
+}
+
+// var nameArray = ['farhad', 'jamal','kamal','damal','shamal'];
+// let nameId = 0;
+// let singlePerson = "";
+// while (nameId < nameArray.length) {
+  
+ 
+//     nameId++;
+    
+// }
+// var num = 0;
+// while (num <= 10) {
+//     console.log(num);
+//     num++
+// }
+
+// odd number list
+
+var oddNum = 1;
+while (oddNum <= 10 ) {
+    console.log(oddNum);
+    oddNum = oddNum +2;
+}
+
+// Even number list
+
+var evenNum = 2;
+while (evenNum <=10) {
+    console.log(evenNum);
+    evenNum = evenNum + 2;
+    
+}
+
+// For loop
+
+
+for (let num = 0; num <= 10; num++) {
+    console.log(num);
+    
+}
+
+// Even number list
+
+for (let num1 = 2; num1 <=10; num1 = num1+2) {
+    console.log(num1);
+    
+}
+
+// odd number list
+for (let num2 = 1; num2 <= 10; num2 =num2+2) {
+    console.log(num2);
+}
+
+// for  loop explor in array
+
+
+const array =[ 67,98,78,54,12,31,32];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+const nameArray = ['farhad', 'jamal','kamal','damal','shamal'];
+
+for (let index = 0; index < nameArray.length; index++) {
+    const element = nameArray[index];
+    console.log(element);
+    
+}
+
+
