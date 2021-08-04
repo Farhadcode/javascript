27b77c2563e925126b5049f2b38de0db36bed332
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-04 07:57:52 +0600

    new addad

---
+++ conditional.js
@@ -0,0 +1,22 @@
+// if (condition) {
+//     // block of code to be executed if the condition is true
+// }
+
+var isFoodReady = true;
+
+if (isFoodReady == true){
+    console.log("Yes baby . I will eat now");
+}
+// The else statement specifies a block of code to be executed if the condition is false:
+var laptopPrice = 70000;
+var myAmount = 65000;
+if (laptopPrice < myAmount){
+    console.log(" I will buy this");
+}
+else if(laptopPrice > myAmount){
+    console.log("Dell i5 is the best");
+
+}
+else{
+    console.log("sorry amount not enough");
+}
