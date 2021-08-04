213935f16524f8834a891c4b7f2219dd4976d1b9
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-04 23:14:51 +0600

    added new

---
+++ nestad-conditon.js
@@ -0,0 +1,31 @@
+var danishPrice = 50;
+var butterBread = 25;
+var toastBiscuit = 10;
+var myBudegt = 30;
+var betterPak = false;
+
+// if(danishPrice <= myBudegt){
+//     console.log('Danish is best for me');
+// }
+// else if (butterBread <= myBudegt ) {
+//     console.log('butter bon  diye  cha khamu');
+// }
+// else if ( toastBiscuit <= myBudegt ){
+//     console.log('toast biscuit khamu');
+// }
+// else{
+//  console.log("amr taka nai");
+// }
+
+if( danishPrice < myBudegt ){
+    if(betterPak == true){
+        console.log(" I need this ");
+    }
+    else{
+        console.log(" no  need this ");
+    }
+}
+else{
+    console.log(" anothe biscuit choos kro");
+}
+
